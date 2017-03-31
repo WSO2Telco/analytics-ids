3027dba2fc76f6d2744c22b2c4b0a65bf25dd2f2
lahiru <lahiru.himasha@gmail.com>
2017-03-31 22:16:18 +0530

    Packing DAS portal jaggery app  when server built

---
+++ product/jaggeryapps/portal/store/carbon.super/fs/gadget/download-he_failure-report/js/main.js
@@ -0,0 +1,137 @@
+var view = {
+    callbacks: [{
+        type: "click",
+        callback: function () {
+            //wso2gadgets.load("chart-1");
+            //  alert("Clicked on bar chart of chart-0. You can implement your own callback handler for this.");
+        }
+    }],
+    subscriptions: [{
+        topic: "subscriber",
+        callback: function (topic, data, subscriberData) {
+            var filter = {
+                timeFrom: data["timeFrom"],
+                timeTo: data["timeTo"],
+                operator: data["operator"],
+                appID: data["appID"]
+            };
+	$('#modalPopup').modal('hide');
+            $( document ).ready(function() {
+                urlAppend(filter);
+            });
+        }
+    }],
+    data: function () {
+        var filter = {};
+       //wso2.gadgets.state.getGlobalState('filter',function(filter) {
+            
+        $( document ).ready(function() {
+            urlAppend(filter);
+        });
+            
+        //});
+    }
+};
+
+function urlAppend(filter){
+    if( filter ==undefined ){
+        filter = {};
+    }
+    var SERVER_URL = "/portal/apis/report";
+    var TYPE_OPERATOR_HE_FAILURE_REPORT=12;
+    $("#downloadcsvdr").attr("href",SERVER_URL+ "?type="+TYPE_OPERATOR_HE_FAILURE_REPORT+"&timeFrom=" + filter["timeFrom"]
+        + "&timeTo=" + filter["timeTo"] + "&operator=" + filter["operator"]
+        + "&appID=" + filter["appID"]+"&download=drXl");
+}
+
+function checkAccess(){
+    var valid=false;
+    var TYPE_OPERATOR_HE_FAILURES=22;
+    var SERVER_URL = "/portal/apis/telcoanalytics";
+    var HTTP_GET = "GET";
+    var RESPONSE_ELEMENT = "responseJSON";
+    jQuery.ajax({
+        url: SERVER_URL + "?type=" + TYPE_OPERATOR_HE_FAILURES,
+        type: HTTP_GET,
+        success: function (data) {
+            var isOperator = JSON.parse(data.message);
+            if(isOperator){
+                document.getElementById('downloadcsvdr').click();
+            }else{
+                $('#modalPopup').modal('hide');
+                $('#popupCheckAccess').modal({
+                    show: 'true'
+                });
+            }
+        },
+        error: function (msg) {
+            error(msg[RESPONSE_ELEMENT]);
+        }
+    });
+    return valid;
+}
+
+
+
+$(function () {
+    try {
+        wso2gadgets.init("#canvas", view);
+        wso2gadgets.load("chart-0");
+
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+gadgets.HubSettings.onConnect = function () {
+    if (view.subscriptions) {
+        view.subscriptions.forEach(function (subscription) {
+            gadgets.Hub.subscribe(subscription.topic, subscription.callback);
+        });
+    }
+};
+
+
+
+function checkLoged(){
+    var valid=false;
+    var TYPE_LOGGIN_CHECK=25;
+    var SERVER_URL = "/portal/apis/telcoanalytics";
+    var HTTP_GET = "GET";
+    var RESPONSE_ELEMENT = "responseJSON";
+    $.ajax({
+        url: SERVER_URL + "?type=" + TYPE_LOGGIN_CHECK,
+        type: HTTP_GET,
+        /*success: function (data) {
+        	
+            var isLogged = JSON.parse(data.message);
+            if(isLogged){
+                document.getElementById('downloadcsvdr').click();
+            }
+        },
+        error: function (msg) {
+        	var ErrorMesssage = JSON.parse(msg.message);
+        									  
+        	 if(ErrorMesssage=="User is not authenticated."){
+        		 console.log("test works correctly")
+             }else{
+            	 document.getElementById('downloadcsvdr').click();
+             }
+        	
+            error(msg[RESPONSE_ELEMENT]);
+        }*/
+        
+        success: function(data, textStatus, xhr) {
+            var isLogged = JSON.parse(data.message);
+            if(isLogged){
+                document.getElementById('downloadcsvdrReal').click();
+            }
+        },
+        complete: function(xhr, textStatus) {
+            if(xhr.status==403){
+            	window.top.location.reload(false);
+            }
+        } 
+    });
+    return valid;
+}
